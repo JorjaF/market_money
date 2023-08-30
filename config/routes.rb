c12b940b1bd2850e8c8ca7094ddb8995afa57a38
Jorja Fleming <aset284@gmail.com>
2023-08-30 15:51:03 -0600

    Complete user story 4

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :markets, only: [:index, :show] do
-        resources :vendors, only: [:index]
+        resources :vendors, only: [:index, :show]
       end
+
+      resources :vendors, only: [:index, :show]
     end
   end
 end
