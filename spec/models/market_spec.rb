dc7b99ec9c2cd23ca447f21841934198802e6e37
Jorja Fleming <aset284@gmail.com>
2023-08-30 14:53:46 -0600

    Completed user story 3; also added more tests

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -19,11 +19,13 @@ RSpec.describe Market, type: :model do
   describe "vendor_count" do
     it "can return a vendor count" do
       market1 = Market.create!(name: "Test Market 1", street: "123 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80202", lat: 39.750833, lon: -104.996944)
+      market2 = Market.create!(name: "Test Market 2", street: "456 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80202", lat: 39.750833, lon: -104.996944)
       
       vendor1 = Vendor.create!(name: "Test Vendor 1", description: "A big ass market", contact_name: "Fred Mertz", contact_phone: "303.555.5521", credit_accepted: "true", market: market1)
       vendor2 = Vendor.create!(name: "Another vendor", description: "We only sell pickles", contact_name: "Marc Kucumber", contact_phone: "303.555.9999", credit_accepted: "true", market: market1)
       
       expect(market1.vendor_count).to eq(2)
+      expect(market2.vendor_count).to eq(0)
     end
   end
 end
--- spec/requests/api/v0/market_api_spec.rb
+++ spec/requests/api/v0/market_api_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Market API", type: :request do
   before(:each) do
-      @market1 = Market.create(name: "Test Market 1", street: "123 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80202", lat: 39.750833, lon: -104.996111)
-      @vendor1 = Vendor.create!(name: "Test Vendor 1", description: "A big ass market", contact_name: "Fred Mertz", contact_phone: "303.555.5521", credit_accepted: true, market: @market1)
-      @vendor2 = Vendor.create!(name: "Another vendor", description: "We only sell pickles", contact_name: "Marc Kucumber", contact_phone: "303.555.9999", credit_accepted: "true", market: @market1)
+      @market1 = Market.create!(name: "Organic Market", street: "123 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80202", lat: 39.750833, lon: -104.996111)
+      @market2 = Market.create!(name: "Simon Whistler Market", street: "456 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80202", lat: 39.750833, lon: -104.996111)
+      @vendor1 = Vendor.create!(name: "Desilu", description: "A big ass market", contact_name: "Fred Mertz", contact_phone: "3031725521", credit_accepted: true, market: @market1)
+      @vendor2 = Vendor.create!(name: "A Raw Dill", description: "We only sell pickles", contact_name: "Marc Kucumber", contact_phone: "303-555-9999", credit_accepted: "true", market: @market1)
+      @vendor3 = Vendor.create!(name: "Chicken Feet", description: "Premium chicken parts for magical use!", contact_name: "Marie Laveau", contact_phone: "504.279.3812", credit_accepted: "true", market: @market1)
     end
 
   describe "GET /api/v0/markets" do
@@ -22,6 +24,9 @@ RSpec.describe "Market API", type: :request do
       expect(market1_data["zip"]).to eq(@market1.zip)
       expect(market1_data["lat"]).to eq(@market1.lat)
       expect(market1_data["lon"]).to eq(@market1.lon)
+
+      market2_data = markets["data"].find { |m| m["id"].to_i == @market2.id }["attributes"]
+      expect(market2_data["name"]).to eq(@market2.name)
     end
   end
 
@@ -68,7 +73,7 @@ RSpec.describe "Market API", type: :request do
       vendors = JSON.parse(response.body)["data"]
 
       expect(vendors).to be_an(Array)
-      expect(vendors.count).to eq(2)
+      expect(vendors.count).to eq(3)
 
       vendor1_data = vendors.find { |v| v["id"].to_i == @vendor1.id }
       
@@ -91,7 +96,30 @@ RSpec.describe "Market API", type: :request do
           "contact_name" => @vendor2.contact_name,
           "contact_phone" => @vendor2.contact_phone,
           "credit_accepted" => @vendor2.credit_accepted,
-      })
+        }) 
+        
+      vendor3_data = vendors.find { |v| v["id"].to_i == @vendor3.id }
+      expect(vendor3_data).to include(
+        "id" => @vendor3.id.to_s,
+        "attributes" => {
+          "name" => @vendor3.name,
+          "description" => @vendor3.description,
+          "contact_name" => @vendor3.contact_name,
+          "contact_phone" => @vendor3.contact_phone,
+          "credit_accepted" => @vendor3.credit_accepted,
+        })
+    end
+
+    it "gives a 404 when there is an invaild market id is input" do
+      get "/api/v0/markets/invalid_id"
+
+      expect(response).to have_http_status(404)
+      
+      json_response = JSON.parse(response.body)
+
+      expect(json_response).to include(
+        "error"=>"Couldn't find Market with 'id'=invalid_id"
+      )
     end
   end
 end
