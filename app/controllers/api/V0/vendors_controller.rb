e224280d327e82e143f4b1c04aa462e6d8b22d0c
Jorja Fleming <aset284@gmail.com>
2023-08-30 17:42:39 -0600

    Complete story 5

--- app/controllers/api/V0/vendors_controller.rb
+++ app/controllers/api/V0/vendors_controller.rb
@@ -13,6 +13,21 @@ module Api
           render json: ErrorSerializer.serialize("Couldn't find Vendor with 'id'=#{params[:id]}"), status: 404
         end
       end
+
+      def create
+        vendor = Vendor.new(vendor_params)
+        if vendor.save
+          render json: VendorSerializer.new(vendor), status: 201
+        else
+          render json: ErrorSerializer.serialize(vendor.errors.messages), status: 400
+        end
+      end
+
+      private
+
+      def vendor_params
+        params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted, :market_id)
+      end
     end
   end
 end
